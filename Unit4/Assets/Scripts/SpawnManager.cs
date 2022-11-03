7db9fccf7a5fb9c1f662c8bccb909a5a20a0b076
LightNeO <antonereminsumy@gmail.com>
2022-11-03 14:39:17 +0200

    End of 4th lesson

--- Unit4/Assets/Scripts/SpawnManager.cs
+++ Unit4/Assets/Scripts/SpawnManager.cs
@@ -6,19 +6,31 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
     private float swapnRange = 9.0f;
+    public int enemyCount;
+
     void Start()
+    
     {
-
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(3);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition()
     {
         float swapnPosX = Random.Range(-swapnRange, swapnRange);
