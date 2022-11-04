42f1e4bf1dfca4eda2d86b95ac8a956953ffc7ab
LightNeO <antonereminsumy@gmail.com>
2022-11-04 16:58:35 +0200

    Lesson 5.1-5.4 Done

--- Project5/Assets/Scripts/Target.cs
+++ Project5/Assets/Scripts/Target.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private Rigidbody targetRb;
+   
     private float minSpeed = 8;
     private float maxSpeed = 14;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = 1;
+
+    private GameManager gameManager;
+    private Rigidbody targetRb;
+    public ParticleSystem exposionParticle;
+
+    public int pointValue;
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -27,6 +34,24 @@ public class Target : MonoBehaviour
         
     }
 
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(exposionParticle, transform.position, exposionParticle.transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
